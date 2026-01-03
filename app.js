a5146f0f4377a9d6723f0a06c7bd5773afc22fa5
Justine M <ilance.mojal@gmail.com>
2026-01-03 21:52:33 +0800

    KAN-3: Test run the sound

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ if (document.readyState !== 'loading') {
 
 function init() {
     // DOM is ready
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease("C4", "8n");
 }
